9f3e06d14966b0f141a78560bd5cc40c8e691d94
JureGrubisic <85846667+JureGrubisic@users.noreply.github.com>
2023-10-09 23:51:10 +0200

    Update add.php

--- Pc Shop/public/products/add.php
+++ Pc Shop/public/products/add.php
@@ -1,1 +1,10 @@
-https://github.com/JureGrubisic/Pc-Shop.git
+<?php
+include("../model/db.php");
+include("../model/product.class.php");
+
+// Call the saveProduct method
+Product::saveProduct($_POST); // Assuming you're sending the product data via POST
+
+// Redirect to a page after adding the product
+header("Location: ../admin-login.php");
+?>
