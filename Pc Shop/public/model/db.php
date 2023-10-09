3ebbd3b73e9a3cc6a947411ee1611cc75b47c346
JureGrubisic <85846667+JureGrubisic@users.noreply.github.com>
2023-10-09 23:37:01 +0200

    Update db.php

--- Pc Shop/public/model/db.php
+++ Pc Shop/public/model/db.php
@@ -1,20 +1,16 @@
 <?php
-    $conn=$hostname = 'localhost'; // Replace with the actual hostname or IP address of your MySQL server.
+    $conn=$hostname = 'localhost'; 
     $username = 'root';
-    $password = ''; // Replace with the actual password for the 'root' user.
-    $database = 'laptopshop'; // Replace with the name of your database.
-    $port = 3306; // Replace with the actual port number if it's different.
+    $password = '';
+    $database = 'pcshop'; 
+    $port = 3306;
     
     $conn = mysqli_connect($hostname, $username, $password, $database, $port);
-    
+    //$conn=mysqli_connect('localhost','fpmoz132023','csdigital2023','fpmoz132023')
     if (!$conn) {
         die('Connection failed: ' . mysqli_connect_error());
     }
     
     ;
-    //$conn=mysqli_connect('localhost','root','','ls_shop');
-
-    //if(!$conn){
-    //    die ("Conection failed!" . mysqli_connect_error());
-    //}
+    
 ?>
