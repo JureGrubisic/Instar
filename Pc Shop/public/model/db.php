8189f32e33775daf4eac550ae0132b173883879b
JureGrubisic <85846667+JureGrubisic@users.noreply.github.com>
2023-10-10 15:00:35 +0200

    Update db.php

--- Pc Shop/public/model/db.php
+++ Pc Shop/public/model/db.php
@@ -2,11 +2,11 @@
     $conn=$hostname = 'localhost'; 
     $username = 'root';
     $password = '';
-    $database = 'pcshop'; 
+    $database = 'pzi132023'; 
     $port = 3306;
     
     $conn = mysqli_connect($hostname, $username, $password, $database, $port);
-    //$conn=mysqli_connect('localhost','fpmoz132023','csdigital2023','fpmoz132023')
+    $conn=mysqli_connect('localhost','pzi132023','csdigital2023','pzi132023')
     if (!$conn) {
         die('Connection failed: ' . mysqli_connect_error());
     }
